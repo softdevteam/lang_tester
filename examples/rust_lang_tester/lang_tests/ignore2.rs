b7cf0cdfda94e0c296cf4acb255505768fa6f98a
Laurence Tratt <laurie@tratt.net>
2024-02-20 12:45:29 +0000

    Document and enforce that `ignore-if` cmds are run in `CARGO_MANIFEST_DIR`.
    This happened implicitly before (unless you did something odd): this
    change makes sure this is the actual behaviour.

---
+++ examples/rust_lang_tester/lang_tests/ignore2.rs
@@ -0,0 +1,8 @@
+// # Always ignore this test
+// ignore-if: cat examples/rust_lang_tester/lang_tests/ignore.rs
+// Compiler:
+//   status: success
+
+fn main() {
+    panic!("Shouldn't happen.");
+}
